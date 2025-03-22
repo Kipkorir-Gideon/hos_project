8589410b422676c2358618428e2ae606f697097f
Ngetich <ngetichgideon21@gmail.com>
2025-03-22 10:44:17 +0300

    Create a trip view using the 'generics.CreateAPIView' class.

--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Trip
+from serializers import TripSerializer
+import requests
+from django.http import JsonResponse
+from datetime import datetime, timedelta
 
-# Create your views here.
+
+class TripCreateView(generics.CreateAPIView):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+    
+
+

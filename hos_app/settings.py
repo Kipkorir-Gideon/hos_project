bbf2263f16b4d909485c0d003f12da5cac08b8a1
Ngetich <ngetichgideon21@gmail.com>
2025-03-27 16:33:04 +0300

    Add Vercel domain to ALLOWED_HOSTS

--- hos_app/settings.py
+++ hos_app/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^klisz7ym-u0x7shex+j039&%n6v(mb!8cjc5*onwczdtfgh%u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
